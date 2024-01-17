4098657c4324d036a60aa9c7b8adf4220d82ffb3
André Carvalho <andreaugustosilvacarvahlo@gmail.com>
2024-01-17 13:28:11 -0300

    printBoardinConsole

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,14 @@
 package application;
-    
+
+import chess.ChessMatch;
 
 public class App {
 
     public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.pieces());
 
     }
     
---
+++ src/boardgame/Board.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board (int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public int getColumn(){
+        return columns;
+    }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+    
+}
