422dfbf2242763c38158317fb669688afd49acdc
André Carvalho <andreaugustosilvacarvahlo@gmail.com>
2024-02-14 00:19:42 -0300

    docs:addingDocumentation

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,6 +24,14 @@ public class Board {
         return columns;
     }
 
+    /**
+     * Get the piece at a specified position on the board.
+     * 
+     * @param row    The row of the position
+     * @param column The column of the position
+     * @return The piece at the specified position
+     * @throws BoardException If the position is not on the board
+     */
     public Piece piece(int row, int column) {
         if (!positionExists(row, column)) {
             throw new BoardException("Position not on the board");
@@ -31,6 +39,13 @@ public class Board {
         return pieces[row][column];
     }
 
+    /**
+     * Get the piece at a specified position on the board.
+     * 
+     * @param position The position on the board
+     * @return The piece at the specified position
+     * @throws BoardException If the position is not on the board
+     */
     public Piece piece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
@@ -38,6 +53,13 @@ public class Board {
         return pieces[position.getRow()][position.getColumn()];
     }
 
+    /**
+     * Place a piece at a specified position on the board.
+     * 
+     * @param piece    The piece to be placed
+     * @param position The position where the piece will be placed
+     * @throws BoardException If the position is occupied or not on the board
+     */
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position)) {
             throw new BoardException("The position is occupied " + position);
@@ -46,14 +68,34 @@ public class Board {
         piece.position = position;
     }
 
+    /**
+     * Check if a position is within the board's limits.
+     * 
+     * @param row    The row of the position
+     * @param column The column of the position
+     * @return true if the position is within the board's limits, false otherwise
+     */
     private boolean positionExists(int row, int column) {
         return (row >= 0) && (row < rows) && (column >= 0) && (column < columns);
     }
 
+    /**
+     * Check if a position is within the board's limits.
+     * 
+     * @param position The position to be checked
+     * @return true if the position is within the board's limits, false otherwise
+     */
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
 
+    /**
+     * Check if there is a piece at a specified position on the board.
+     * 
+     * @param position The position to be checked
+     * @return true if there is a piece at the specified position, false otherwise
+     * @throws BoardException If the position is not on the board
+     */
     public boolean thereIsAPiece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
@@ -61,6 +103,14 @@ public class Board {
         return piece(position) != null;
     }
 
+    /**
+     * Remove a piece from a specified position on the board.
+     * 
+     * @param position The position of the piece to be removed
+     * @return The removed piece, or null if there is no piece at the specified
+     *         position
+     * @throws BoardException If the position is not on the board
+     */
     public Piece removePiece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -13,12 +13,25 @@ public abstract class Piece {
         return board;
     }
 
+    /**
+     * Returns a matrix representing the possible movements of the piece.
+     * @return A boolean matrix where true indicates a possible movement and false indicates an impossible movement.
+     */
     public abstract boolean[][] possibleMovies();
 
+    /**
+     * Checks if there is a possible move for the piece at a specific position.
+     * @param position The position to be checked
+     * @return true if there is a possible move at the specified position, false otherwise
+     */
     public boolean possibleMovie(Position position) {
         return possibleMovies()[position.getRow()][position.getColumn()];
     }
 
+    /**
+     * Checks if there is any possible move for the piece.
+     * @return true if there is at least one possible move, false otherwise
+     */
     public boolean isThereAnyPossibleMove() {
         boolean[][] mat = possibleMovies();
         for (int i = 0; i < mat.length; i++) {
